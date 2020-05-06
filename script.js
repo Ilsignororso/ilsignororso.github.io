86892af0da50bd9fa2237bef47b294b02141adac
Ilsignororso <64487345+Ilsignororso@users.noreply.github.com>
2020-05-06 17:17:13 +0200

    Bunchofstuff

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Mission Success!");
+
+let printNumbers = (n) => {
+    for (let i = 1; i <= n; i++) {
+      console.log(i);
+    }
+}
+
+let greetingName = (name) => {
+  console.log('Hello ' + name + "!")
+}
+
+let myNumbers = [10, 20, 30, 45, 50];
+
+myNumbers.forEach((number) => {
+  console.log(number);
+});
